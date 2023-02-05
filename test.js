2d163b7a94ca1c8b74cad4170895199d875812e5
Your Name <you@example.com>
2023-02-05 20:46:56 +0530

    First Big Commit

---
+++ test.js
@@ -0,0 +1,11 @@
+// console.log("hI");
+// let letterArray=[{name:"Unnikuttan",place:"mukkada"},{name:"athul",place:"manakkadavu"}]
+
+// letterArray.forEach((letter)=>{
+//     console.log(letter.name+", "+letter.place);
+// })
+
+var texxt = "Hello world"
+console.log(texxt);
+texxt = texxt.substring(0,8);
+console.log(texxt+"...")
